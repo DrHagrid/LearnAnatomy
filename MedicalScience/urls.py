e46d4e4c8198043d28d4cf6dcf18e9eb6d16ee20
DrHagrid <mail.hagrid@gmail.com>
2018-08-05 14:20:48 +0300

    v.1.3 - release

--- MedicalScience/urls.py
+++ MedicalScience/urls.py
@@ -1,9 +1,11 @@
+from django.views.generic.base import RedirectView
 from django.contrib.auth.views import login, logout
 from django.conf.urls import url, include
 from django.contrib import admin
 from users import views
 
 urlpatterns = [
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico', permanent=True)),
     url(r'^accounts/login/$', login, name='login'),
     url(r'^accounts/logout/$', logout, name='logout'),
     url(r'^accounts/register/$', views.RegisterFormView.as_view(), name='register'),
